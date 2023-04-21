8c9b75da8f0d14ff57a249ab148841ecb15b72e9
Aquila Sands <5774614+AquilaSands@users.noreply.github.com>
2023-04-21 02:20:20 +0100

    chore: typo fix in comment

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,7 +4,7 @@ inputs_path='./inspec/inputs.generated.yml'
 
 # Read the terraform outputs as JSON,
 # filter the data using jq to extract the required values in the required format,
-# and use Ruby to convert to YAML then save the result in a temporrary file to pass to inspec exec.
+# and use Ruby to convert to YAML then save the result in a temporary file to pass to inspec exec.
 terraform output -json -state=../src/terraform/terraform.tfstate |
 jq "{parent_id: .parent.value, top_level_folder_name: .top_level_folder_name.value, environment_folder_names: [.environment_folders.value[].display_name], team_folder_names: ([.team_folders.value[].display_name] | unique)}" |
 ruby -ryaml -rjson -e 'puts YAML.dump(JSON.parse(STDIN.read))' > "$inputs_path"
